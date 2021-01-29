645d63e701b36fb66097909f572fecd5a18c9501
Rakesh Suryawanshi <67486994+krishrocks1904@users.noreply.github.com>
2021-01-29 19:08:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = requires('express');
+var http = require("http");
 
-const app = express();
-
-app.get('/',(req,res)=>{
-res.send('<h1>My App </h1>');
-});
-
-app.listen(5000, ()=>{
-console.log('App Listening on port 5000 !');
-});
+http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+ }).listen(8081);
+ 
+ // Console will print the message
+ console.log('Server running at http://127.0.0.1:8081/');
